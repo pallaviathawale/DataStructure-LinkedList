09e808610006505f2bb08faeb617ad2f3e02dd5c
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-16 13:33:38 +0530

    [PALLAVI] Add: InsertionAtPerticularPosition

--- DataStructure-LinkedList/LinkedList.cs
+++ DataStructure-LinkedList/LinkedList.cs
@@ -6,55 +6,80 @@ using System.Threading.Tasks;
 
 namespace DataStructure_LinkedList
 {
-    class LinkedList
-    {
-        public Node head;
-        public void InsertLast(int new_data)
+   class LinkedList
         {
+            internal Node head;
+            internal void Add(int data)
+            {
+                Node node = new Node(data); //object of new and push data into node
+                if (this.head == null)
+                    this.head = node;
 
-            Node new_node = new Node(new_data);
-            if (this.head == null)
-            {
-                this.head = new_node;
-            }
-            else
-            {
-                Node lastNode = GetLastNode();
-                lastNode.next = new_node;
-            }
-            Console.WriteLine("inserted Last into list " + new_node.data);
+                else
+                {
 
-        }
-        public Node GetLastNode()
-        {
-            Node temp = this.head;
-            while (temp.next != null)
-            {
-                temp = temp.next;
-            }
-            return temp;
-        }
-        public void Append(int new_data)
-        {
-            InsertLast(new_data);
-        }
+                    Node temp = head;
+                    while (temp.next != null)
+                    {
+                        //Console.WriteLine("temp value is " +temp.next.data);
+                        temp = temp.next;
 
-    
-    internal void Display()
-        {
-            Node temp = this.head;
-            if (temp == null)
-            {
-                Console.WriteLine("Linked list is empty");
-                return;
+                    }
+                    temp.next = node;
+                }
+                Console.WriteLine("{0} inserted into the linked list", node.data);
+                // Console.WriteLine("Value of node is: " + node);
             }
-            while (temp != null)
+
+            internal void Display()
             {
-                Console.Write(temp.data + " ");
-                temp = temp.next;
+                Node temp = this.head;
+
+                if (temp == null)
+                {
+                    Console.WriteLine("Linked list is empty");
+                    return;
+                }
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
             }
+            internal Node InsertAtPerticularPosition(int position, int data)
+            {
+                if (position < 1)
+                    Console.WriteLine("Invalid Position");
+
+                if (position == 1)
+                {
+                    var newNode = new Node(data);
+                    newNode.next = this.head;
+                    head = newNode;
+                }
+                else
+                {
+                    while (position-- != 0)
+                    {
+                        if (position == 1)
+                        {
+                            Node node = new(data);
+                            node.next = this.head.next;
+                            head.next = node;
+                            break;
+
+                        }
+                        head = head.next;
+                    }
+                    if (position != 1)
+                        Console.WriteLine("position out of range");
+                }
+                Console.WriteLine("inserted value is " + head);
+                return head;
+
+            }
+
 
         }
     }
-}
 
--- DataStructure-LinkedList/Program.cs
+++ DataStructure-LinkedList/Program.cs
@@ -5,11 +5,11 @@
         static void Main(string[] args)
         {
             LinkedList list = new LinkedList();
-            list.Append(56);
-            list.Append(30);
-            list.Append(70);
-            list.Display();
-            Console.ReadLine();
+            list.Add(56);
+            //list.Add(30);
+            list.Add(70);
+            //list.Display();
+            list.InsertAtPerticularPosition(2, 30);
         }
     }
     
